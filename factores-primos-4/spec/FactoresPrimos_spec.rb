7842ba7d60df327c721fd075ec22ed4e265a9b38
rocioechegaraysierra <rocio.echegaray.sierra@gmail.com>
2017-05-28 21:35:13 -0300

    Test de factores-primos-4

--- factores-primos-4/spec/FactoresPrimos_spec.rb
+++ factores-primos-4/spec/FactoresPrimos_spec.rb
@@ -6,8 +6,20 @@ describe 'FactoresPrimosTest' do
 	let(:factores) { FactoresPrimos.new }
 
 	begin
-		it 'primos de 9 son 3 y 3 ' do
+		it 'factores de 9 son 3 y 3 ' do
 			expect(factores.solucion("9")).to eq([3, 3])
+		end
+
+		it 'factores de 14 son 2 y 7 ' do
+			expect(factores.solucion("14")).to eq([2, 7])
+		end
+
+		 it 'factores de 360 son 2, 2, 2, 3, 3 y 5' do
+			expect(factores.solucion("360")).to eq([2, 2, 2, 3, 3, 5])
+		end
+		
+		 it 'Letra no es valida y lanza una excepcion' do
+			expect{calculadora.calcular("R")}.to raise_exception
 		end
 	end
 end
