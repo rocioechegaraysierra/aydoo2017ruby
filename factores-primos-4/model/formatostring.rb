367a7995cb6583636026d410d6cf5fd03d55fb31
rocioechegaraysierra <rocio.echegaray.sierra@gmail.com>
2017-06-09 10:05:25 -0300

    Se implementa el formatear array

--- factores-primos-4/model/formatostring.rb
+++ factores-primos-4/model/formatostring.rb
@@ -1,1 +1,19 @@
-
+class FormatoString
+    
+  def formatear(array)
+    
+    arraystring = ""
+    
+    array.each do |x|
+      arraystring += x.to_s + ","
+    end
+    
+    if arraystring.length > 0
+      arraystring = arraystring[0...-1]
+    end
+    
+    return arraystring
+    
+  end
+  
+end
